427aa66e8c827088055a9ed90a99eddf302c0111
ned rihine <ned.rihine@gmail.com>
2015-01-24 11:41:55 +0900

    WIndows のデフォルト文字セットを iso-2022-jp に変更
      - nemerle.el を追加

--- init-availables/000-locale.el
+++ init-availables/000-locale.el
@@ -7,7 +7,8 @@
 ;;; Char code:
 (if windows-nt-p
     (let ((default-coding 'japanese-shift-jis-dos)
-          (system-coding 'utf-16le-dos))
+          (system-coding 'iso-2022-jp)
+          (clipboard-coding 'utf-16le-dos))
       ;;
       (when (fboundp 'set-w32-system-coding-system)
         (set-w32-system-coding-system system-coding))
@@ -15,7 +16,7 @@
       (when (fboundp 'set-default-coding-systems)
         (set-default-coding-systems default-coding))
       ;; デフォルトのファイルバッファの文字コードを設定します。
-      (setq default-buffer-file-coding-system default-coding)
+      (setq buffer-file-coding-system default-coding)
       ;; 端末の文字コードです。
       (set-terminal-coding-system default-coding)
       ;; Windows NT の内部文字コードは UTF-16LE なので、クリップボードのエンコードを utf-16le-dos にしておきます。
@@ -30,7 +31,7 @@
     (when (fboundp 'set-default-coding-systems)
       (set-default-coding-systems default-coding))
     ;; デフォルトのファイルバッファの文字コードを設定します。
-    (setq default-buffer-file-coding-system default-coding)
+    (setq buffer-file-coding-system default-coding)
     ;; 端末の文字コードです。
     (set-terminal-coding-system default-coding)
     ;; クリップボードのエンコードです。
