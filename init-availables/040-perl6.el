2df225911877bca76b6ea4707ddc71f786a32c38
ned rihine <ned.rihine@gmail.com>
2016-01-10 16:49:15 +0900

    add package perl6-mode

--- init-availables/040-perl6.el
+++ init-availables/040-perl6.el
@@ -1,8 +1,6 @@
-;;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil; lexical-binding: t; -*-
+;;; 040-perl6.el ---                                 -*- lexical-binding: t; -*-
 
-;;; 040-programing-language.el ---
-
-;; Copyright (C) 2014  ned rihine
+;; Copyright (C) 2016  ned rihine
 
 ;; Author: ned rihine <ned.rihine@gmail.com>
 ;; Keywords: 
@@ -25,24 +23,11 @@
 ;; 
 
 ;;; Code:
-(load "040-c-common")
-(load "040-lisp-common")
+(lazyload (perl6-mode) "perl6-mode")
+;; Perl6 モード。
+(push '("\\.pl6$" . perl6-mode) auto-mode-alist)
+(push '("\\.pm6$" . perl6-mode) auto-mode-alist)
 
-(load "040-ceylon")
-(load "040-csharp")
-(load "040-coffee-script")
-(load "040-type-script")
-(load "040-d")
-(load "040-elm")
-(load "040-dart")
-(load "040-haxe")
-(load "040-haskell")
-(load "040-perl6")
-(load "040-ocaml")
-(load "040-ruby")
-(load "040-vala")
-(load "040-scala")
-(load "040-powershell")
 
-(provide '040-programing-language)
-;;; 040-programing-language.el ends here
+(provide '040-perl6)
+;;; 040-perl6.el ends here
