6b0ed6565907926e7d28c2d7e7c3e412009a0fde
ned rihine <ned.rihine@gmail.com>
2018-07-29 17:27:34 +0900

    コード関係のフォントが違うので修正するコードを追加

--- init-availables/010-markdown.el
+++ init-availables/010-markdown.el
@@ -1,13 +1,20 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil; -*-
 
 ;;; Code:
-(push '("\\.md$" . markdown-mode) auto-mode-alist)
-(push '("\\.mrd$" . markdown-mode) auto-mode-alist)
-(push '("\\.mard$" . markdown-mode) auto-mode-alist)
+(push '("\\.md$"       . markdown-mode) auto-mode-alist)
+(push '("\\.mrd$"      . markdown-mode) auto-mode-alist)
+(push '("\\.mard$"     . markdown-mode) auto-mode-alist)
 (push '("\\.markdown$" . markdown-mode) auto-mode-alist)
 
 (setq markdown-command "markded")
 
-(lazyload (markdown-mode) "markdown-mode")
+(lazyload (markdown-mode) "markdown-mode"
+ ;; 最近の markdown-mode はコードら辺のフォントファミリーが違うので、同じにする。
+ ;; しかしながら、グローバルなフォント設定と異なる可能性があるため、その辺が課題である。
+ (let ((font-family-name "Ricty")
+       (font-size        (* 10 10)))
+   (set-face-attribute 'markdown-code-face        nil :family font-family-name :height font-size)
+   (set-face-attribute 'markdown-inline-code-face nil :family font-family-name :height font-size)
+   (set-face-attribute 'markdown-code-face        nil :family font-family-name :height font-size)))
 
 (provide '010-markdown)
